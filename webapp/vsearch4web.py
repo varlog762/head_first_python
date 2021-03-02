aa63b8bb3e891153c2245ad0827ab6216ba95c10
Gregory Pereverzev <boot762@gmail.com>
2021-03-02 16:51:42 +0300

    Добавлено логирование.

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, escape
 from utils.vsearch import search4letters
 
 app = Flask(__name__)
+
+
+def log_request(req: "flask_requesrt", res: str) -> None:
+    with open('vsearch.log', 'a') as log:
+        print(str(dir(req)), res, file=log)
 
 
 @app.route('/vsearch4', methods=['POST'])
@@ -12,6 +17,7 @@ def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
     results = str(search4letters(phrase, letters))
+    log_request(request, results)
     return render_template('results.html', the_title=title, the_phrase=phrase, the_letters=letters,
                            the_results=results)
 
@@ -20,6 +26,13 @@ def do_search() -> 'html':
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcome to search4letter on the web!')
+
+
+@app.route('/viewlog')
+def view_the_log() -> str:
+    with open("vsearch.log") as log:
+        contents = escape(log.read())
+    return contents
 
 
 if __name__ == '__main__':
