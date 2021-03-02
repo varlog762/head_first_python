0a254137d5b09d0217b77fec1acbae50ba2f2390
Gregory Pereverzev <boot762@gmail.com>
2021-03-02 14:57:12 +0300

    Доработана функция do_search - теперь она принимает аргументы из пользовательского вводо. Настрон редирект с /.

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,17 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from utils.vsearch import search4letters
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello() -> str:
-    return 'Hello world from Flask!'
+def hello() -> '302':
+    return redirect('/entry')
 
 
-@app.route('/search4')
-def do_search() -> str:
-    return str(search4letters('life, the universe, and everything', 'eiru,1'))
+@app.route('/vsearch4', methods=['POST'])
+def do_search() -> 'html':
+    """Функция принимает данные отправленные через веб-форму и возвращает результат: request.form['phrase'] - фраза в
+    которой будем искать нужные символы, request.form['letters'] - нужные символы."""
+    title = 'Here are your results:'
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    results = str(search4letters(phrase, letters))
+    return render_template('results.html', the_title=title, the_phrase=phrase, the_letters=letters,
+                           the_results=results)
 
 
 @app.route('/entry')
@@ -19,4 +26,4 @@ def entry_page() -> 'html':
     return render_template('entry.html', the_title='Welcome to search4letter on the web!')
 
 
-app.run()
+app.run(debug=True)
