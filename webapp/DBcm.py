a2be5f433bde58cb84a15c88ae2ad45ef0066f3c
Gregory Pereverzev <boot762@gmail.com>
2021-03-16 12:37:52 +0300

    Добавлена многопоточность. Доработана функция vsearch4letters - к параметрам phrase и letters применен метод .lower()

--- webapp/DBcm.py
+++ webapp/DBcm.py
@@ -5,6 +5,14 @@ class ConnectionError(Exception):
     pass
 
 
+class CredentialsError(Exception):
+    pass
+
+
+class SQLError(Exception):
+    pass
+
+
 class UseDataBase:
     def __init__(self, dbconfig: dict) -> None:
         self.dbconfig = dbconfig
@@ -16,8 +24,14 @@ class UseDataBase:
             return self.cursor
         except mysql.connector.errors.InterfaceError as err:
             raise ConnectionError(err)
+        except mysql.connector.errors.ProgrammingError as err:
+            raise ConnectionError(err)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.conn.commit()
         self.cursor.close()
         self.conn.close()
+        if exc_type is mysql.connector.errors.ProgrammingError:
+            raise SQLError(exc_val)
+        elif exc_type:
+            raise exc_type(exc_val)
